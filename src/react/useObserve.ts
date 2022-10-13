4987a5c83344e67d3a75c94e41c82897b1b8d421
Jay Meistrich <jmeistrich@gmail.com>
2022-10-13 15:51:15 +0800

    Add an optional callback to useObserve which gets called when it changes

--- src/react/useObserve.ts
+++ src/react/useObserve.ts
@@ -1,11 +1,18 @@
 import { observe, Selector, computeSelector } from '@legendapp/state';
 import { useEffect, useRef } from 'react';
 
-export function useObserve<T>(selector: Selector<T>): void {
-    const ref = useRef<Selector<T>>();
-    ref.current = selector;
+export function useObserve<T>(selector: Selector<T>, callback?: (value: T) => void): void {
+    const ref = useRef<{ selector: Selector<T>; callback: (value: T) => void }>();
+    ref.current = { selector, callback };
 
     useEffect(() => {
-        return observe(() => computeSelector(ref.current));
+        let shouldCallback = false;
+        return observe(() => {
+            const value = computeSelector(ref.current.selector);
+            if (shouldCallback) {
+                ref.current.callback?.(value);
+            }
+            shouldCallback = !!callback;
+        });
     }, []);
 }
